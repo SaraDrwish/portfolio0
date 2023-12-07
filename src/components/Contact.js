46c30373f05413c8946039467e97707ea1154349
SaraDrwish <38770404+SaraDrwish@users.noreply.github.com>
2023-12-07 21:41:39 +0300

    45

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4a954a6025678e811cdb8505dbaec88f09a21cac96f67111dec4289b4a106b2b
-size 4354
+oid sha256:cc460088f83e68e94cb7befe77e638d4c20f849696ccdd2da23a9195daa35c63
+size 4519
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:70d78afdcbab7dc4c519618f89cfe4fafee28c1d39d70041baf0fd0919bc6eec
-size 12538
+oid sha256:48a69156a885033ae32744c35fd5abfdf29399f55eb920c44c7b1487f64524b5
+size 12540
