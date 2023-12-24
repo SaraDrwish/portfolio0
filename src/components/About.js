ebb23c10cea8ff2856a37eda595255bad8891571
SaraDrwish <38770404+SaraDrwish@users.noreply.github.com>
2023-12-24 23:49:12 +0300

    del img abt and banner matbe

--- src/components/About.js
+++ src/components/About.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:39723764518016feaa478c7b2543ee4cd937ba4e3f13680f5f38e642db543957
-size 3844
+oid sha256:dac3787de5b723dcbff54493f54c40c4c2750e0754ca534118f92f9ca5cff948
+size 4159
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79041efc56d5488755ab4a54d357fbea013e20fc30ca1a0d6ba02d5d6d54ffd0
-size 4542
+oid sha256:01b74df51fca4d8f11172f2319b26af2d2116d1f4ecfa676d01b8f492adb152a
+size 4937
